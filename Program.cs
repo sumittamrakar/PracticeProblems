21a76d27d5dccd7217947abe237971e46f2d2ad1
Sumit Tamrakar <tamrakarSumit7@gmail.com>
2020-07-28 23:40:19 -0600

    First two problems

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // FirstNonRecurring("BBCABA");
+            // longestConsecutiveChar("AABCDDBBBEA");
+        }
+
+        private static int FirstNonRecurring(string test)
+        {
+            char[] count = new char[256];
+            for (int i = 0; i < test.Length; i++)
+            {
+                count[test[i]]++;
+            }
+
+            int index = -1;
+            for (int i = 0; i < test.Length; i++)
+            {
+                if (count[test[i]] == 1)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            var x = test[index];
+            return x;
+        }
+
+        private static Dictionary<char, int> longestConsecutiveChar(string test)
+        {
+            int max_count = 0;
+            char max_char = '\0';
+            char prev_char = '\0';
+            int count = 0;
+
+            foreach (char current in test)
+            {
+                if (current == prev_char)
+                {
+                    count++;
+                }
+                else
+                {
+                    count = 1;
+                }
+                if (count > max_count)
+                {
+                    max_count = count;
+                    max_char = current;
+                }
+                prev_char = current;
+            }
+            return new Dictionary<char, int>
+            {
+                { max_char, max_count }
+            };
+        }
+    }
+}
